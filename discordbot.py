8140950b54189e51af8a4cd23b207716962af6d1
9rd <53429209+9rd@users.noreply.github.com>
2019-07-30 21:54:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,5 +19,40 @@ async def ping(ctx):
 async def bosyuu(ctx):
     await ctx.send('pong')
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('みゃおん')
+        # メンバーのリストを取得して表示
+    if message.content == '/members':
+        print(message.guild.members)
+        # 役職のリストを取得して表示
+    if message.content == '/roles':
+        print(message.guild.roles)
+        # テキストチャンネルのリストを取得して表示
+    if message.content == '/text_channels':
+        print(message.guild.text_channels)
+        # ボイスチャンネルのリストを取得して表示
+    if message.content == '/voice_channels':
+        print(message.guild.voice_channels)
+        # カテゴリチャンネルのリストを取得して表示
+    if message.content == '/category_channels':
+        print(message.guild.categories)
+    if message.content == '/team':
+        vc = client.get_channel(CHANNEL_ID)
+        members = vc.members
+        print(members)
+
 
 bot.run(token)
