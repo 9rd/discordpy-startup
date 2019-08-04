6c8f03e06fe4c786f5f4e3821dbbbd6897b957c6
9rd <53429209+9rd@users.noreply.github.com>
2019-08-04 19:00:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,8 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+vc = client.get_channel(600258156001624064)
+members = vc.members
 
 
 @bot.event
@@ -14,5 +16,13 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    if message.content == '/team''
+        print(members)
 
 bot.run(token)
